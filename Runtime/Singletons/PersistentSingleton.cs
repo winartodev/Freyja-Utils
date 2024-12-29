0d1b171ff2cacfe88c1a7c2e0f1e6f793ea7f3c8
Winarto <winarto007zing@gmail.com>
2024-12-29 07:31:52 +0700

    Initial Commit

---
+++ Runtime/Singletons/PersistentSingleton.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+namespace Freyja.Utils
+{
+    public abstract class PersistentSingleton<T> : MonoBehaviour where T : Component
+    {
+        #region Static
+
+        protected static T instance;
+
+        public static bool HasInstance
+        {
+            get => instance != null;
+        }
+
+        public static T TryGetInstance()
+        {
+            return HasInstance ? instance : null;
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindAnyObjectByType<T>();
+                    if (instance == null)
+                    {
+                        var go = new GameObject(typeof(T).Name + "_AutoCreated");
+                        instance = go.AddComponent<T>();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        public bool AutoUnparentOnAwake = true;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Make sure to call base.Awake() in override if you need awake.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected virtual void InitializeSingleton()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            if (AutoUnparentOnAwake)
+            {
+                transform.SetParent(null);
+            }
+
+            if (instance == null)
+            {
+                instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                if (instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Runtime/Singletons/Singleton.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace Freyja.Utils
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Component
+    {
+        #region Static
+
+        protected static T instance;
+
+        public static bool HasInstance
+        {
+            get => instance != null;
+        }
+
+        public static T TryGetInstance()
+        {
+            return HasInstance ? instance : null;
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindAnyObjectByType<T>();
+                    if (instance == null)
+                    {
+                        var go = new GameObject(typeof(T).Name + "_AutoCreated");
+                        instance = go.AddComponent<T>();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Make sure to call base.Awake() in override if you need awake.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected virtual void InitializeSingleton()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            instance = this as T;
+        }
+
+        #endregion
+    }
+}
